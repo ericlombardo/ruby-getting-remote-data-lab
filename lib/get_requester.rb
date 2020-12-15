adeb847e372a7bc00484fce867da66bf6e107d91
Eric <eric.m.lombardo@gmail.com>
2020-12-15 14:33:07 -0500

    commit tree 1f53c7a95d10a16da1d2e3a71406cdd71651e565

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,23 @@
-# Write your code here
+class GetRequester
+  require 'pry'      
+  require 'open-uri'  # makes url link a URI object => can use .open #=> StringIO
+  require 'net/http'  # makes requests to endpoints #=> NET::HTTPOK object
+  require 'json'      # takes NET::HTTPOK object and formats for easier manipulatioin
+  attr_accessor :url
+
+  def initialize(url)
+    @url = url    # assign url to variable
+  end
+
+  def get_response_body
+    uri = URI.parse(@url)   # make uri object
+    response = Net::HTTP.get_response(uri)    # send request with NET::HTML
+    response.body   #=> body from Net::HTTPOK object returned 
+  end
+
+  def parse_json    #=> formatted body returned by #get_response_body method
+    JSON.parse(get_response_body)
+  end
+
+
+end
